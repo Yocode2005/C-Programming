26f4e2061ed13821e9e709a9c821b33c66328018
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-03-28 22:25:00 +0530

    Add Node class implementation with main function demonstrating linked list creation and traversal

---
+++ 32-linkedlist/02-nodePointer.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Node{ // class
+    public:
+    int val;
+     Node* next;
+     Node(int val){ // constructor
+        this->val = val;
+        this->next = NULL;
+     }
+};
+int main(){
+   Node* a = new Node(10);
+   Node* b = new Node(20);
+   Node* c = new Node(30);
+   Node* d = new Node(40);
+   a->next = b;
+   b->next = c;
+   c->next = d;
+   cout<<a->next->next->next->val<<endl; // to print the value of d with the help of a
+   Node* temp = a;
+   while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp = temp->next;
+   }
+}
