84ef91b02e61eb299dcafa3d6266d890ec6b06d3
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-21 17:15:14 +0530

    construct an dubly linkedlist

--- 33-DubblyLinkedList/01-dubbleLLManualImplementation.cpp
+++ 33-DubblyLinkedList/01-dubbleLLManualImplementation.cpp
@@ -11,6 +11,29 @@ class Node{
         this->prev = NULL; // initialize the prev pointer to NULL in the constructor
     }
 };
+void display(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->val<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
 int main(){
-
+    Node*a = new Node(10);
+    Node*b = new Node(20);
+    Node*c = new Node(30);
+    Node*d = new Node(40);
+    Node*e = new Node(50);
+    // linking the nodes together to form a doubly linked list
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    // linking the previous pointers
+    b->prev = a;
+    c->prev = b;
+    d->prev = c;
+    e->prev = d;
+    display(a);
 }
