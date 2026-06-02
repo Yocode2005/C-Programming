dfac1395af356ff0783c4cb99bec4b6c4119404e
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-02 10:33:00 +0530

    start learning heaps

---
+++ 34-Heaps/01basicMaxHeap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    priority_queue<int> maxHeap; // in c++ by default it is max heap
+    maxHeap.push(1);
+    maxHeap.push(2);
+    maxHeap.push(3);
+    maxHeap.push(4);
+    maxHeap.push(5);
+
+    cout<<"size of heap is : "<<maxHeap.size()<<endl; // size of heap is : 5
+    cout<<"top element is : "<<maxHeap.top()<<endl; // top element is : 5//  always return the maximum element in the heap
+}
