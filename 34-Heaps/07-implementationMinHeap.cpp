0dbc43f9d3683ac5147f8636249db7f9b8e81480
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-05 09:44:53 +0530

    implementation of minheap

---
+++ 34-Heaps/07-implementationMinHeap.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class MinHeap{
+    public : 
+    int arr[50];
+    int idx ;
+    MinHeap(){
+        idx = 1;
+    }
+    int top(){
+        return arr[1];
+    }
+    void push(int x){
+        arr[idx] = x;
+        int i = idx;
+        while(i>1){
+            int parent = i/2;
+            if(arr[parent]>arr[i]){
+                swap(arr[parent],arr[i]);
+                i = parent;
+            }
+            else{
+                break;
+            }
+        }
+        idx++;
+    }
+};
+int main(){
+    MinHeap pq;
+    pq.push(10);
+    pq.push(20);
+    pq.push(11);
+    pq.push(9);
+    cout<<pq.top()<<endl;
+}
