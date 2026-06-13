fdbfb1d360c3aec5def22f0bd5b25f69b20710c5
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-13 21:52:49 +0530

    update

--- 01-Basics/ArithmeticOperators.cpp
+++ 01-Basics/ArithmeticOperators.cpp
@@ -4,7 +4,7 @@ int main(){
      cout<<"hello world"<<endl;
      cout<<"hello yogesh"<<endl;
      int x=5;
-     cout<<x<<endl;
+     cout<<x<<endl<<endl;
      cout<<++x<<endl<<endl;
      cout<<x<<endl;
     
