55b5af729cfb9ca408fd23efaa07f6c394c17ccd
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-13 21:53:46 +0530

    add one more line

--- 01-Basics/ArithmeticOperators.cpp
+++ 01-Basics/ArithmeticOperators.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-     cout<<"hello world"<<endl;
+     cout<<"hello world"<<endl<<endl;
      cout<<"hello yogesh"<<endl;
      int x=5;
      cout<<x<<endl<<endl;
