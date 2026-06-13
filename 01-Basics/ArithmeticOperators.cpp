afb59927471905794d9dff6e644b0cec74d178e4
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-13 21:54:11 +0530

    fix the bugs

--- 01-Basics/ArithmeticOperators.cpp
+++ 01-Basics/ArithmeticOperators.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
      cout<<"hello world"<<endl<<endl;
-     cout<<"hello yogesh"<<endl;
+     cout<<"hello yogesh"<<endl<<endl;
      int x=5;
      cout<<x<<endl<<endl;
      cout<<++x<<endl<<endl;
