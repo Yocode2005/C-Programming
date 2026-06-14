666564c2f2048f1c1f42d1aa93faf3f8f75ae1ea
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-14 22:00:17 +0530

    update

--- 06-Pattern/solidRectangle.cpp
+++ 06-Pattern/solidRectangle.cpp
@@ -9,7 +9,7 @@ int main(){
     cout<<"number of rows : ";
     cin>>n;
     int m;
-    cout<<"number of columns :";
+    cout<<"number of columns:";
     cin>>m;
     for(int i=1;i<=n;i++){ // number of rows ke liye use kiya
         for(int j=1;j<=m;j++){// number of columns or number of stars ke liye use kiya
