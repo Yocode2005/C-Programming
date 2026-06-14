b96aa9cf7fdba224570bc7557ff857219f5f1b48
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-14 22:01:51 +0530

    update

--- 06-Pattern/starCross.cpp
+++ 06-Pattern/starCross.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"enter the number of rows : ";
+    cout<<"enter the number of rows: ";
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
