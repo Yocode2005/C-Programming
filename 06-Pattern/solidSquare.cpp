831590d94e2de25cc02df00131bf805383f2ea31
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-14 22:00:40 +0530

    update

--- 06-Pattern/solidSquare.cpp
+++ 06-Pattern/solidSquare.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"enter the number of rows : ";
+    cout<<"enter the number of rows: ";
     cin>>n;
     for(int i=1;i<=n;i++){
         // jb bhi ek loop ke andr dusra loop use kre to dono loop ke itrrater diff. hone chaiye 
