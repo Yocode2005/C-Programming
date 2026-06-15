6e3802d2dfa745ba2367d15c47d211917b0966ff
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-15 22:48:00 +0530

    add space

--- 07-Pattern/oddStarTriangle.cpp
+++ 07-Pattern/oddStarTriangle.cpp
@@ -11,7 +11,7 @@ int main(){
     cin>>n;
     for(int i = 1;i<=n;i++){
         for(int s = 1;s<=n-i;s++){
-            cout<<" ";
+            cout<<"  ";
         }
         for(int j=1;j<=2*i-1;j++){
             cout<<"*";
