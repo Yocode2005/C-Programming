d5b180dec7f38a0a03c28ed4f2b76a4fff041f5e
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-15 22:45:47 +0530

    add space

--- 07-Pattern/numberPyramidPalindrome.cpp
+++ 07-Pattern/numberPyramidPalindrome.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
     for(int i = 1;i<=n;i++){
         for(int s =1;s<=n-i;s++ ){
-            cout<<" ";
+            cout<<"  ";
         }
         for(int j  =1;j<=i;j++){
             cout<<j;
