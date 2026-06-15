9768a0cd2fbfd7150b85570eed3e25a2aea8a335
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-15 22:46:44 +0530

    update

--- 07-Pattern/numberSpiral.cpp
+++ 07-Pattern/numberSpiral.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"enter the number of lines : ";
+    cout<<"enter the number of lines: ";
     cin>>n; 
     for(int i =1;i<=2*n-1;i++){
         for(int j =1;j<=2*n-1;j++){
